5e5cc568c5462ee27a99b4a67deaa38f28f407ab
1nature <olanipekun1439@yahoo.com>
2025-03-10 16:10:50 +0000

    Deleted comment code in Constant.cs

--- Me_MoApp/Constants.cs
+++ Me_MoApp/Constants.cs
@@ -2,8 +2,6 @@
 {
     public static class Constants
     {
-        //public const string savedPostPath = @"the_stored_post.xml"; //not working
-
-        public const string savedPostPath = "the_stored_post"; //worked
+        public const string savedPostPath = "the_stored_post"; 
     }
 }
