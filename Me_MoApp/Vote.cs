607dac3cecce2bd3165715807610c94cb7f11c87
1nature <olanipekun1439@yahoo.com>
2025-04-09 23:21:20 +0100

    Created properties for upvoting and downvoting in the Vote class

--- Me_MoApp/Vote.cs
+++ Me_MoApp/Vote.cs
@@ -13,8 +13,37 @@
         public int TotalVote  //TODO: either +1 or -1
         {
             get { return _totalvote; }
-            set { _totalvote = value; }
+            set
+            {
+                _totalvote = Vot.UpVote + Vot.DownVote;
+                //_totalvote = value;
+            }
         }
+
+        private int _upvote;
+
+        public int UpVote //TODO: either +1 or -1
+        {
+            get { return _upvote; }
+            set { _upvote = value; }
+        }
+
+        private int _downvote;
+
+        public int DownVote //TODO: either +1 or -1
+        {
+            get { return _downvote; }
+            set
+            {
+                _downvote = -1;
+                //if (value < 0)
+                //{
+                //    _downvote = value;
+                //}
+            }
+        }
+
+
 
         private List<Vote> _votes = new List<Vote>(); //this list stores all votes
         public List<Vote> Votes //User is not known when their vote is added to the list
@@ -23,28 +52,36 @@
             set { _votes = value; }
         }
 
-        public int TotalVotes
+        //public int TotalVotes
+        //{
+        //    get
+        //    {
+        //        int totalvotes = 0;
+        //        foreach (Vote vo in _votes)
+        //        {
+        //            totalvotes += vo.TotalVote;
+        //        }
+
+        //        return totalvotes;
+        //    }
+        //}
+
+        //public void CalculateVote(int points, User u)
+        //{
+        //    Vote userVote = new();
+        //    userVote.TimeStamp = DateTime.Now;
+        //    userVote.User = u;
+        //    userVote.TotalVote = points;
+
+        //    _votes.Add(userVote);
+        //}
+
+        private Vote _vot;
+
+        public Vote Vot
         {
-            get
-            {
-                int totalvotes = 0;
-                foreach (Vote vo in _votes)
-                {
-                    totalvotes += vo.TotalVote;
-                }
-
-                return totalvotes;
-            }
-        }
-
-        public void CalculateVote(int points, User u)
-        {
-            Vote userVote = new();
-            userVote.TimeStamp = DateTime.Now;
-            userVote.User = u;
-            userVote.TotalVote = points;
-
-            _votes.Add(userVote);
+            get { return _vot; }
+            set { _vot = value; }
         }
 
 
