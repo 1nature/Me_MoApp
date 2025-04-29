b90ab6533d7e3f8e73f14d1df5a50ad089262440
1nature <olanipekun1439@yahoo.com>
2025-04-29 10:25:16 +0100

    Added the path for saving users in the constant class

--- Me_MoApp/Constants.cs
+++ Me_MoApp/Constants.cs
@@ -2,6 +2,8 @@
 {
     public static class Constants
     {
-        public const string savedPostPath = "the_stored_post"; 
+        public const string savedPostPath = "the_stored_post";
+        public const string savedUserPath = "the_stored_user";
+
     }
 }
