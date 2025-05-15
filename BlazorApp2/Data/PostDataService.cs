9535d5a91e5091e791acc4b53fad2d103719ac5e
1nature <olanipekun1439@yahoo.com>
2025-05-15 13:57:59 +0100

    added new project with identity enabled

---
+++ BlazorApp2/Data/PostDataService.cs
@@ -0,0 +1,48 @@
+﻿using Me_MoApp;
+
+namespace BlazorApp1.Data
+{
+    public class PostDataService
+    {
+        public PostDataService()
+        {
+            //   Posts = GetTestPosts(); //post has data
+            //  LogicMethod.SaveDataToDisk(Posts); //post is empty, what am I missing?
+
+            Posts = LogicMethod.LoadPostDataFromDisk(); //TODO: implement DE-serialisation
+            //put a condition to check if file exist like the quiz maker
+            //like a method exist
+
+            Comments = GetTestComments();
+        }
+
+        public List<Post> Posts { get; set; } = new();
+
+        public List<Post> GetTestPosts()
+        {
+            var posts = TestData.MakePosts();
+            return posts;
+        }
+
+
+        public List<Comment> Comments { get; set; } = new();
+
+        public List<Comment> GetTestComments()
+        {
+            var comments = TestData.MakeComments();
+            return comments;
+        }
+
+        public int TotalAmountOfVotes()
+        {
+            int total = 0;
+            foreach (Post p in Posts)
+            {
+                total += p.TotalVotes;
+            }
+            return total;
+
+        }
+
+    }
+}
