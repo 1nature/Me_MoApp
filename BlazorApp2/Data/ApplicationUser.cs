3dfdabf85332e689626909b5a7ceaaeab5150631
1nature <olanipekun1439@yahoo.com>
2025-05-28 11:54:25 +0100

    Added more use attributes to the applicationuser class

--- BlazorApp2/Data/ApplicationUser.cs
+++ BlazorApp2/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace BlazorApp2.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? FavouriteColor { get; set; }
+        public DateTime BirthDate { get; set; }
+
     }
 
 }
